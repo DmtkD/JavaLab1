2a281171cd73dba65e26ae80177b4fefa5b03211
DmtkD <pavlo.to474@gmail.com>
2022-02-22 20:30:52 +0200

    Update

--- Lab1/src/ua/lviv/IoT/lab1/TouristPass.java
+++ Lab1/src/ua/lviv/IoT/lab1/TouristPass.java
@@ -17,15 +17,6 @@ public record TouristPass(String country, int during, int price,
     this(country, during, price, "Buss", 10, 400);
   }
 
-  public TouristPass(String country, int during, int price, String transport, int amount, int insurance) {
-    this.country = country;
-    this.during = during;
-    this.price = price;
-    this.transport = transport;
-    this.amount = amount;
-    this.insurance = insurance;
-  }
-
   @Override
   public String toString() {
     return String.format("You gonna to %s by %s and this will cost %d$ for %d people at %d days.",
